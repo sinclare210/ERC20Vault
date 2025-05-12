303ae5a6ee3889745e9395e23ce5229ccc2da3db
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-12 16:05:04 +0100

    Almost done

--- src/Vault.sol
+++ src/Vault.sol
@@ -21,6 +21,7 @@ contract Vault {
     function approve(uint256 _amount) public {
         if (msg.sender == address(0)) revert InvalidAddress();
         if (_amount <= 0) revert ZeroNotAllowed();
+        if(token.balanceOf(msg.sender) <= _amount) revert InsufficientFunds();
         
         token.approve(address(this), _amount);
     }
@@ -29,6 +30,7 @@ contract Vault {
         if (msg.sender == address(0)) revert InvalidAddress();
         if (_amount <= 0) revert ZeroNotAllowed();
         if(token.allowance(msg.sender, address(this)) <= _amount) revert InsufficientFunds();
+
         (bool success) = token.transferFrom(msg.sender, address(this), _amount);
         require(success, "Transfer Failed");
         balances[msg.sender] = balances[msg.sender] + _amount;
