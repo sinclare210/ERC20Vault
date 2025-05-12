427edc8373ba8a3dd6d4c9ca5bfe3bbaa75f2560
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-12 16:19:53 +0100

    Almost done

--- src/Vault.sol
+++ src/Vault.sol
@@ -6,49 +6,40 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Vault {
     IERC20 public immutable token;
 
-    mapping(address => uint256) balances;
-
-    uint256 contractBalance;
+    mapping(address => uint256) public balances;
+    uint256 public contractBalance;
 
     error InvalidAddress();
     error ZeroNotAllowed();
-    error InsufficientFunds();
+    error InsufficientAllowance();
+    error InsufficientVaultBalance();
 
     constructor(address _token) {
         if (_token == address(0)) revert InvalidAddress();
         token = IERC20(_token);
     }
 
-    function approve(uint256 _amount) public {
-        if (msg.sender == address(0)) revert InvalidAddress();
-        if (_amount <= 0) revert ZeroNotAllowed();
-        if(token.balanceOf(msg.sender) <= _amount) revert InsufficientFunds();
-        
-        token.approve(address(this), _amount);
-    }
-
     function deposit(uint256 _amount) public {
         if (msg.sender == address(0)) revert InvalidAddress();
-        if (_amount <= 0) revert ZeroNotAllowed();
-        if(token.allowance(msg.sender, address(this)) < _amount) revert InsufficientFunds();
+        if (_amount == 0) revert ZeroNotAllowed();
+        if (token.balanceOf(msg.sender) < _amount) revert InsufficientAllowance();
 
-        (bool success) = token.transferFrom(msg.sender, address(this), _amount);
-        require(success, "Transfer Failed");
-        balances[msg.sender] = balances[msg.sender] + _amount;
-        contractBalance = contractBalance + _amount;
+        bool success = token.transferFrom(msg.sender, address(this), _amount);
+        require(success, "Transfer failed");
+
+        balances[msg.sender] += _amount;
+        contractBalance += _amount;
     }
 
     function withdraw(uint256 _amount) public {
         if (msg.sender == address(0)) revert InvalidAddress();
-        if (_amount <= 0) revert ZeroNotAllowed();
-        if(balances[msg.sender] <= _amount) revert InsufficientFunds();
+        if (_amount == 0) revert ZeroNotAllowed();
+        if (balances[msg.sender] < _amount) revert InsufficientVaultBalance();
 
-        balances[msg.sender] = balances[msg.sender] - _amount;
+        balances[msg.sender] -= _amount;
+        contractBalance -= _amount;
 
-        contractBalance = contractBalance - _amount;
-
-        (bool success) = token.transfer(msg.sender, _amount);
-
-        require(success, "Failed");
+        bool success = token.transfer(msg.sender, _amount);
+        require(success, "Withdraw transfer failed");
     }
 }
