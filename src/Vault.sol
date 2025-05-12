4ccedef14aa53f7795938e46fabb77ac39ee4249
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-13 00:33:30 +0100

    Almost done

--- src/Vault.sol
+++ src/Vault.sol
@@ -9,7 +9,6 @@ contract Vault {
     mapping(address => uint256) public balances;
     uint256 public contractBalance;
 
-    error InvalidAddress();
     error ZeroNotAllowed();
     error InsufficientAllowance();
     error InsufficientVaultBalance();
@@ -18,20 +17,10 @@ contract Vault {
     event Withdrawn(address indexed user, uint256 amount);
 
     constructor(address _token) {
-        if (_token == address(0)) revert InvalidAddress();
         token = IERC20(_token);
     }
 
-    // function approve(uint256 _amount) public {
-    //     if (msg.sender == address(0)) revert InvalidAddress();
-    //     if (_amount <= 0) revert ZeroNotAllowed();
-    //     if(token.balanceOf(msg.sender) <= _amount) revert InsufficientFunds();
-
-    //     token.approve(address(this), _amount);
-    // }
-
     function deposit(uint256 _amount) public {
-        if (msg.sender == address(0)) revert InvalidAddress();
         if (_amount == 0) revert ZeroNotAllowed();
         if (token.allowance(msg.sender, address(this)) < _amount) revert InsufficientAllowance();
 
@@ -45,7 +34,6 @@ contract Vault {
     }
 
     function withdraw(uint256 _amount) public {
-        if (msg.sender == address(0)) revert InvalidAddress();
         if (_amount == 0) revert ZeroNotAllowed();
         if (balances[msg.sender] < _amount) revert InsufficientVaultBalance();
 
