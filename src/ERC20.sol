6cedda3d73eb361eab8f143d5d6b1a544284e18b
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-12 14:23:25 +0100

    starting

---
+++ src/ERC20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+
+contract Sinclair is ERC20, ERC20Permit {
+    constructor(address recipient)
+        ERC20("Sinclair", "SIN")
+        ERC20Permit("Sinclair")
+    {
+        _mint(recipient, 100000000000 * 10 ** decimals());
+    }
+}
