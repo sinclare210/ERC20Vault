2db71af17d72c94b0465e59ccfca974a3f7ea7d9
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-12 15:05:44 +0100

    cooking

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,4 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+contract Vault {
+
+    IERC20 public immutable token;
+
+    constructor (address _token){
+        token = IERC20(_token);
+    }
+
+    function allowance() internal view {
+        token.allowance(msg.sender, address(this));
+
+    }
+
+    function approve (uint256 _amount) internal {
+        token.approve(msg.sender, _amount);
+    }
+
+    funtion 
+
+
+
+
+
+}
