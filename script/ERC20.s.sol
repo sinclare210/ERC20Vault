f170263024849b14169f27fab864ee758bb364c0
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-14 05:38:39 +0100

    Done with deploy

--- script/ERC20.s.sol
+++ script/ERC20.s.sol
@@ -2,3 +2,15 @@
 pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
+
+import {Sinclair} from "../src/ERC20.sol";
+
+contract ERC20Script is Script{
+    Sinclair public sinclair;
+    function run() external{
+        vm.startBroadcast();
+        sinclair = new Sinclair(msg.sender);
+        vm.stopBroadcast();
+    }
+
+}
--- script/Vault.s.sol
+++ script/Vault.s.sol
@@ -2,3 +2,16 @@
 pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
+
+import {Vault} from "../src/Vault.sol";
+
+contract VaultScript is Script{
+    
+    Vault public vault;
+    function run() external{
+        vm.startBroadcast();
+        vault = new Vault(0x3F2ce71A3e46d57C89ECDE315D7b382CE20D600a);
+        vm.stopBroadcast();
+    }
+
+}
