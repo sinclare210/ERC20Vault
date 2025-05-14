9432bf678069809319b07684719e931eed2307e3
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-15 00:03:47 +0100

    Writing tests

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -4,31 +4,65 @@ pragma solidity ^0.8.19;
 import {Test, console} from "forge-std/Test.sol";
 import {Vault} from "../src/Vault.sol";
 
+import {Sinclair} from "../src/ERC20.sol";
+
 contract VaultTest is Test {
     Vault public vault;
+    Sinclair public sinclair;
+
+    address sinc = address(0x1);
 
     function setUp() external {
-        vault = new Vault(0x3F2ce71A3e46d57C89ECDE315D7b382CE20D600a);
+        
+        vm.startPrank(sinc);
+        sinclair = new Sinclair(sinc);
+        sinclair.unRestrictedMint(sinc, 100000000000 * 10 ** 18);
+        vm.stopPrank();
+        // vault = new Vault(0x3F2ce71A3e46d57C89ECDE315D7b382CE20D600a);
+        vault = new Vault(address(sinclair));
+        uint256 balance = sinclair.balanceOf(sinc);
+        console.log("sinc Balance:", balance);
     }
 
     function testDepositSuccess() public {
         // Should deposit tokens successfully
+        
+
+
+
+
     }
 
     function testDepositZeroReverts() public {
         // Should revert when deposit amount is zero
+        vm.startPrank(sinc);
+        vm.expectRevert(Vault.ZeroNotAllowed.selector);
+        vault.deposit(0);
+        vm.stopPrank();
+
+        
     }
 
     function testDepositWithoutApprovalReverts() public {
         // Should revert if no approval is given
+        vm.startPrank(sinc);
+        vm.expectRevert(Vault.InsufficientAllowance.selector);
+        vault.deposit(100000);
+        vm.stopPrank();
     }
 
     function testDepositInsufficientApprovalReverts() public {
         // Should revert if approved amount is less than deposit
+        vm.startPrank(sinc);
+        sinclair.approve(address(this), 100000);
+        vm.expectRevert(Vault.InsufficientAllowance.selector);
+        vault.deposit(10000000);
+        vm.stopPrank();
     }
 
     function testDepositFailsIfTransferFromReturnsFalse() public {
         // Should revert if token’s transferFrom returns false
+        
     }
 
     function testWithdrawSuccess() public {
