dd8f5fb7ef5b5dcca6799007269ea7e11fa9ee40
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-15 01:00:24 +0100

    Not Done

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -10,14 +10,17 @@ contract VaultTest is Test {
     Vault public vault;
     Sinclair public sinclair;
 
-    uint256 public constant approveAmount = 1000000000000000000000;
+    uint256 public constant approveAmount = 10000000000000000000000;
     uint256 public constant depositAmount = 100000000000000000000;
+    uint256 public constant withdrawAmount = 10000000000000000000;
 
     event Deposited(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
 
     address sinc = address(0x1);
+    address sinc2 = address(0x2);
 
-    modifier approveAndDeposit (){
+    modifier approveAndDeposit() {
         vm.startPrank(sinc);
         sinclair.approve(address(vault), approveAmount);
         vault.deposit(depositAmount);
@@ -25,21 +28,39 @@ contract VaultTest is Test {
         _;
     }
 
+    modifier approveAndDeposit2() {
+        vm.startPrank(sinc2);
+        sinclair.approve(address(vault), approveAmount);
+        vault.deposit(depositAmount);
+        vm.stopPrank;
+        _;
+    }
+
     function setUp() external {
+        sinclair = new Sinclair(address(this));
+        vm.startPrank(sinc2);
+
+        sinclair.unRestrictedMint(sinc2, 100000000000 * 10 ** 18);
+        vm.stopPrank();
+
         vm.startPrank(sinc);
-        sinclair = new Sinclair(sinc);
+
         sinclair.unRestrictedMint(sinc, 100000000000 * 10 ** 18);
         vm.stopPrank();
+
         // vault = new Vault(0x3F2ce71A3e46d57C89ECDE315D7b382CE20D600a);
         vault = new Vault(address(sinclair));
         uint256 balance = sinclair.balanceOf(sinc);
         console.log("sinc Balance:", balance);
+
+        uint256 balance2 = sinclair.balanceOf(sinc2);
+        console.log("sinc2 Balance:", balance2);
     }
 
     function testDepositSuccess() public approveAndDeposit {
         // Should deposit tokens successfully
         vm.startPrank(sinc);
-        
+
         assertEq(vault.balances(sinc), depositAmount);
         assertEq(vault.contractBalance(), depositAmount);
 
@@ -48,7 +69,6 @@ contract VaultTest is Test {
     }
 
     function testDepositZeroReverts() public {
-        // Should revert when deposit amount is zero
         vm.startPrank(sinc);
         vm.expectRevert(Vault.ZeroNotAllowed.selector);
         vault.deposit(0);
@@ -56,7 +76,6 @@ contract VaultTest is Test {
     }
 
     function testDepositWithoutApprovalReverts() public {
-        // Should revert if no approval is given
         vm.startPrank(sinc);
         vm.expectRevert(Vault.InsufficientAllowance.selector);
         vault.deposit(100000);
@@ -74,13 +93,17 @@ contract VaultTest is Test {
 
     function testDepositFailsIfTransferFromReturnsFalse() public {
         // Should revert if token’s transferFrom returns false
-        
     }
 
-    function testWithdrawSuccess() public approveAndDeposit{
+    function testWithdrawSuccess() public approveAndDeposit {
         // Should withdraw deposited tokens successfully
+        vm.startPrank(sinc);
+        vault.withdraw(withdrawAmount);
+        assertEq(vault.balances(sinc), depositAmount - withdrawAmount);
+        assertEq(vault.contractBalance(), depositAmount - withdrawAmount);
 
-
+        emit Vault.Withdrawn(sinc, withdrawAmount);
+        vm.stopPrank();
     }
 
     function testWithdrawZeroReverts() public {
@@ -89,10 +112,9 @@ contract VaultTest is Test {
         vm.expectRevert(Vault.ZeroNotAllowed.selector);
         vault.withdraw(0);
         vm.stopPrank();
-
     }
 
-    function testWithdrawMoreThanBalanceReverts() public approveAndDeposit{
+    function testWithdrawMoreThanBalanceReverts() public approveAndDeposit {
         // Should revert when withdrawing more than deposited
         vm.startPrank(sinc);
         vm.expectRevert(Vault.InsufficientVaultBalance.selector);
@@ -104,15 +126,27 @@ contract VaultTest is Test {
         // Should revert if token’s transfer returns false
     }
 
-    
-
-    function testMultipleDepositsAccumulateCorrectly() public {
+    function testMultipleDepositsAccumulateCorrectly() public approveAndDeposit approveAndDeposit2 {
         // Repeated deposits by one user should be cumulative
+        assertEq(vault.balances(sinc), depositAmount);
+        assertEq(vault.balances(sinc2), depositAmount);
+        assertEq(vault.contractBalance(), depositAmount * 2);
     }
 
-    function testMultipleUsersTrackedIndependently() public {
+    function testMultipleUsersTrackedIndependently() public approveAndDeposit approveAndDeposit2 {
         // Separate users can deposit/withdraw independently
-    }
+        vm.startPrank(sinc);
+        vault.withdraw(withdrawAmount);
+        emit Vault.Withdrawn(sinc, withdrawAmount);
+        vm.stopPrank();
 
-    
+        vm.startPrank(sinc2);
+        vault.withdraw(withdrawAmount);
+        emit Vault.Withdrawn(sinc2, withdrawAmount);
+        vm.stopPrank();
+
+        assertEq(vault.balances(sinc), depositAmount - withdrawAmount);
+        assertEq(vault.balances(sinc2), depositAmount - withdrawAmount);
+        assertEq(vault.contractBalance(), 2 * (depositAmount - withdrawAmount));
+    }
 }
