43600c68c59aec8d58341621cd9b107ad1107566
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-16 15:25:17 +0100

    doings

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -3,10 +3,10 @@
 pragma solidity ^0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Sinclair is ERC20, ERC20Permit {
-    constructor(address recipient) ERC20("Sinclair", "SIN") ERC20Permit("Sinclair") {
+
+contract Sinclair is ERC20 {
+    constructor(address recipient) ERC20("Sinclair", "SIN")  {
         _mint(recipient, 100000000000 * 10 ** decimals());
     }
 
